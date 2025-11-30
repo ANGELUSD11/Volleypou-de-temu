52cffa035da96d9d0bdc0d09f3a3125abd88b3f3
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-11-30 17:43:24 -0500

    fix colliders in main

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -8,6 +8,9 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("restart"):
 		restart_current_scene() # Replace with function body.
 
-
 func restart_current_scene() -> void:
 	get_tree().reload_current_scene()
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ball"):
+		get_tree().call_deferred("reload_current_scene")
--- Scripts/pelota.gd
+++ Scripts/pelota.gd
@@ -1,43 +1,25 @@
 extends RigidBody2D
 
-#raycast reference
-@onready var raycast: RayCast2D = $RayCast2D
-var max_speed: float = 1000.0
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var radio: float = 30.0
+var max_speed: float = 200.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	freeze = true # Replace with function body.
-	raycast.top_level = true
-	
+	pass
+
 func _physics_process(delta: float) -> void:
-	if freeze:
-		return
-		
-	raycast.global_position = global_position
-	raycast.target_position = linear_velocity * delta * 1.5
-	raycast.force_raycast_update()
-	
-	if raycast.is_colliding():
-		var normal = raycast.get_collision_normal()
-		
-		if linear_velocity.dot(normal) < 0:
-			_forzar_rebote_manual(normal)
-			
-func _forzar_rebote_manual(normal_pared):
-	var velocidad_actual = linear_velocity
-	var velocidad_rebote = velocidad_actual.bounce(normal_pared)
-	
-	linear_velocity = velocidad_rebote
-	global_position += normal_pared * 2.0
-	
-func _integrate_forces(state):
-	if state.linear_velocity.length() > max_speed:
-		state.linear_velocity = state.linear_velocity.normalized() * max_speed
+	# 1. Rotación Visual (Cosmético)
+	if not freeze:
+		var velocidad_angular = linear_velocity.x / radio
+		sprite.rotation += velocidad_angular * delta
+	else:
+		return # Si está congelada, no calculamos nada más
 
 func _on_body_entered(body: Node) -> void:
 	if freeze == true:
 		if body.is_in_group("player"):
 			move_ball()
-		
+
 func move_ball():
 	set_deferred("freeze", false)
