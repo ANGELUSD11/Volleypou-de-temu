3f19db9b9f1044993792894dcf626eae2f43a33d
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-11-30 18:25:48 -0500

    change ticks per second = 120

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+@onready var reload_timer: Timer = $DetectionArea/ReloadTimer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	reload_timer.timeout.connect(restart_current_scene)
 	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("restart"):
@@ -13,4 +15,4 @@ func restart_current_scene() -> void:
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("ball"):
-		get_tree().call_deferred("reload_current_scene")
+		reload_timer.start()
--- Scripts/pelota.gd
+++ Scripts/pelota.gd
@@ -8,14 +8,6 @@ var max_speed: float = 200.0
 func _ready() -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
-	# 1. Rotación Visual (Cosmético)
-	if not freeze:
-		var velocidad_angular = linear_velocity.x / radio
-		sprite.rotation += velocidad_angular * delta
-	else:
-		return # Si está congelada, no calculamos nada más
-
 func _on_body_entered(body: Node) -> void:
 	if freeze == true:
 		if body.is_in_group("player"):
