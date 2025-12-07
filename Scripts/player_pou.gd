30997a6773bfaa092bca2ce74dd126c630f85c39
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2025-12-07 14:08:10 -0500

    player 2 controls input maps

--- Scripts/player_pou.gd
+++ Scripts/player_pou.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+@export var left_button: String = "P1_left"
+@export var right_button: String = "P1_right"
+@export var jump_button: String = "P1_jump"
+
 var SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
@@ -10,12 +14,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("P1_jump") and is_on_floor():
+	if Input.is_action_just_pressed(jump_button) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("P1_left", "P1_right")
+	var direction := Input.get_axis(left_button, right_button)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
